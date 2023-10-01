13c41e467f87b980358443849e36fd91b9830f5c
ArtemPika <orelartem228@gmail.com>
2023-10-01 13:14:30 +0300

    Lab2: Implemented step 2

--- Lab2/src/main/java/org/matrix/Matrix.java
+++ Lab2/src/main/java/org/matrix/Matrix.java
@@ -5,6 +5,28 @@ public class Matrix {
     private int colCount;
     private double[][] data;
 
+    public Matrix() {
+        rowCount = 0;
+        colCount = 0;
+        data = new double[0][0];
+    }
+    public Matrix(int rowCount, int colCount) {
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+        this.data = new double[rowCount][colCount];
+    }
+    public Matrix(Matrix matrix) {
+        rowCount = matrix.getRowCount();
+        colCount = matrix.getColCount();
+        double[][] dataToCopy = matrix.getData();
+        data = new double[rowCount][colCount];
+        for (int i = 0; i < rowCount; ++i) {
+            for (int j = 0; j < colCount; ++j) {
+                data[i][j] = dataToCopy[i][j];
+            }
+        }
+    }
+
 
     public int getRowCount() {
         return rowCount;
--- Lab2/src/test/java/MatrixTest.java
+++ Lab2/src/test/java/MatrixTest.java
@@ -1,6 +1,28 @@
 import org.junit.jupiter.api.Test;
 import org.matrix.Matrix;
 
-public class MatrixTest {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class MatrixTest {
+    @Test
+    public void ShouldCreateEmptyMatrix() {
+        Matrix A = new Matrix();
+        assert (A.getData().length == 0);
+    }
+    @Test
+    public void ShouldCreateMatrixByGivenDimensions() {
+        Matrix A = new Matrix(3, 2);
+        assert (A.getData().length == 3) && (A.getData()[0].length == 2);
+    }
+    @Test
+    public void ShouldCreateMatrixFromAnotherMatrix() {
+        Matrix A = new Matrix(3, 2);
+        Matrix B = new Matrix(A);
+
+        assert (A.getRowCount() == B.getRowCount());
+        assert (A.getColCount() == B.getColCount());
+        assertArrayEquals(A.getData(), B.getData());
+    }
 }
